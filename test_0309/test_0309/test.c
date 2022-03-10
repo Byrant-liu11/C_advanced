0c16470cc3360ffe8946a94386ee451de789c125
Byrant-liu11 <lk5425864@163.com>
2022-03-10 22:23:22 +0800

    指针进阶笔记

---
+++ test_0309/test_0309/test.c
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//
+//#include<stdio.h>
+//
+//int main()
+//{
+//	//void* 的指针是无具体类型的指针
+//	//可以接收任意类型数据的地址
+//	//void*的指针不能直接+-整数的操作
+//	//void*的指针不能直接解引用操作
+//
+//}
